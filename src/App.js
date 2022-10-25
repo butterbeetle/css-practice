8635ce6ea68af5cb788fd10b37f3c31d080efba7
butterbeetle <butterbeetle@naver.com>
2022-10-26 00:43:12 +0900

    [2] 준비

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className={classes.productOverview}>
+        <h1>Get the freedom you deserve.</h1>
+      </section>
+      <section className={classes.plans}>
+        <h1 className={classes.sectionTitle}>Choose Your Plan</h1>
+        <p>Make sure you get the most for your money!</p>
+      </section>
+    </main>
   );
 }
 

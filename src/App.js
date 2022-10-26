2a10ebc802fa0f5650e0f4d8f606e87d8f15223c
butterbeetle <butterbeetle@naver.com>
2022-10-26 18:36:06 +0900

    [3] CSS 좀 더 알아보기

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import classes from "./App.module.css";
 function App() {
   return (
     <main>
+      <header className={classes.mainHeader}>
+        <div>
+          <a href="/">uHost</a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Packages</a>
+            </li>
+            <li>
+              <a href="/">Customers</a>
+            </li>
+            <li className={classes.cta}>
+              <a href="/">Start Hosting</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <section className={classes.productOverview}>
         <h1>Get the freedom you deserve.</h1>
       </section>
+
       <section className={classes.plans}>
         <h1 className={classes.sectionTitle}>Choose Your Plan</h1>
         <p>Make sure you get the most for your money!</p>

9cf3d3d994eadebf27ea06f35b88ce5d91301d86
butterbeetle <butterbeetle@naver.com>
2022-10-28 00:34:21 +0900

    [20] fixed 연습

--- src/examplar/examplar.js
+++ src/examplar/examplar.js
@@ -3,9 +3,9 @@ import classes from "./examplar.module.css";
 const Examplar = () => {
   return (
     <div className={classes.parent}>
-      <div className={classes.parent}>Navigation Bar</div>
-      <div className={classes.parent}>Background Image</div>
-      <div className={classes.parent}>Features</div>
+      <div className={classes.child1}>Navigation Bar</div>
+      <div className={classes.child2}>Background Image</div>
+      <div className={classes.child3}>Features</div>
     </div>
   );
 };

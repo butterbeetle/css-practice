194f8ce7cacea3302344958ead46079c7e499807
butterbeetle <butterbeetle@naver.com>
2022-10-30 23:32:08 +0900

    [48] flex 이론 연습용

--- src/examplar/examplar.js
+++ src/examplar/examplar.js
@@ -4,20 +4,13 @@ import classes from "./examplar.module.css";
 const Examplar = () => {
   return (
     <Fragment>
-      <div className={classes.parent}>
-        <div className={classes.country}>Navigation Bar</div>
-        <div className={classes.cities}>Background Image</div>
-        <div className={classes.cities}>Features</div>
-      </div>
-      <div className={classes.parent}>
-        <div className={classes.country}>Navigation Bar</div>
-        <div className={classes.cities}>Background Image</div>
-        <div className={classes.cities}>Features</div>
-      </div>
-      <div className={classes.parent}>
-        <div className={classes.country}>Navigation Bar</div>
-        <div className={classes.cities}>Background Image</div>
-        <div className={classes.cities}>Features</div>
+      <div className={classes["flex-container"]}>
+        <div className={classes["item-1"]}>div</div>
+        <div className={classes["item-2"]}>w=250px</div>
+        <div className={classes["item-3"]}>h=250px</div>
+        <div className={classes["item-4"]}>w/h=300px</div>
+        <div className={classes["item-5"]}>w=350px</div>
+        <div className={classes["item-6"]}>w=350px</div>
       </div>
     </Fragment>
   );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Packages from "./packages/packages";
-// import Examplar from "./examplar/examplar";
+import Examplar from "./examplar/examplar";
 import Customers from "./components/Customers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,7 +14,7 @@ root.render(
       <Route path="/" element={<App />} />
       <Route path="/packages" element={<Packages />} />
       <Route path="/customers" element={<Customers />} />
-      {/* <Route path="/examplar" element={<Examplar />} /> */}
+      <Route path="/examplar" element={<Examplar />} />
     </Routes>
   </BrowserRouter>
 );

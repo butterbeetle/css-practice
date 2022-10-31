e4ac2c70c6034ae4d3774cdeaf9fbe102d34a7a3
butterbeetle <butterbeetle@naver.com>
2022-10-31 22:50:21 +0900

    [61] gap 활용해서 간격 주기

--- src/examplar/GridExam.js
+++ src/examplar/GridExam.js
@@ -2,19 +2,19 @@ import classes from "./GridExam.module.css";
 
 const GridExam = () => {
   return (
-    // <div className={classes.container}>
-    //   <div className={classes.el1}>Element 1</div>
-    //   <div className={classes.el2}>Element 2</div>
-    //   <div className={classes.el3}>Element 3</div>
-    //   <div className={classes.el4}>Element 4</div>
-    // </div>
-
     <div className={classes.container}>
-      <div className={classes.header}>Header</div>
-      <div className={classes.sidebar}>Sidebar</div>
-      <div className={classes.mainContent}>Main Content</div>
-      <div className={classes.footer}>Footer</div>
+      <div className={classes.el1}>Element 1</div>
+      <div className={classes.el2}>Element 2</div>
+      <div className={classes.el3}>Element 3</div>
+      <div className={classes.el4}>Element 4</div>
     </div>
+
+    // <div className={classes.container}>
+    //   <div className={classes.header}>Header</div>
+    //   <div className={classes.sidebar}>Sidebar</div>
+    //   <div className={classes.mainContent}>Main Content</div>
+    //   <div className={classes.footer}>Footer</div>
+    // </div>
   );
 };
 
